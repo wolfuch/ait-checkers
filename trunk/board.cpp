17784d814c8c034c9037c03ed27609d386ba12bc
strigare <strigare@162a5ca2-ab1d-11dd-b9e7-8d5594d729c2>
2008-11-19 10:31:24 +0000

    Little changes to board, organized code and added a undoMove to the hpp. WhiteCheckers and BlackCheckers are now organized as an array

--- trunk/board.cpp
+++ trunk/board.cpp
@@ -2,17 +2,17 @@
 /*
  * checkers
  * Copyright (C)  2008 <>
- * 
+ *
  * checkers is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * checkers is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -21,41 +21,46 @@
 #include "board.hpp"
 
 //constructor
-board::board()
+board::board() //READY!
 {
-	
+
 	boardMatrix = new int*[9]; //creates a pointer array (matrix)
 	for (int i=0; i<9; i++)//initializes the array in order to create a matrix
 		boardMatrix[i] = new int[9];
-	
-	
+
+
 	for(int i=0;i<9;i++) //fill it with zeros just in case
 		for(int j=0;j<9;j++)
 			boardMatrix[i][j]=0;
-	
+
 	for(int i=1;i<4;i++)//We put the white checkers on the board
 	{
 		for(int j=i%2+1;j<9;j+=2)
 		{
-			boardMatrix[i][j]=1;
+			boardMatrix[i][j]=WHITEMAN;
 			whiteCheckers.insert(i*10+j); //we add the checkers to the list
 		}
 	}
-	
+
 	for(int i=6;i<9;i++)//We put the black checkers on the board
 	{
-		
+
 		for(int j=i%2+1;j<9;j+=2)
 		{
-			boardMatrix[i][j]=2;
+			boardMatrix[i][j]=BLACKMAN;
 			blackCheckers.insert(i*10+j);//we add the checkers to the blackCheckers list
 		}
 	}
-	
+
+	checkers[WHITE]=whiteCheckers;
+	checkers[BLACK]=blackCheckers;
+
+	kings[WHITE]=whiteKings;
+	kings[BLACK]=blackKings;
 }
 
 //Prints the board
-void board::printBoard()
+void board::printBoard()// READY!
 {
 	for(int i=8;i>0;i--)
 	{
@@ -66,27 +71,27 @@ void board::printBoard()
 }
 
 //returns the board
-int** board::getBoard() 
+int** board::getBoard() // READY!
 {
 	return boardMatrix;
 }
 
 //checks if the position is inside the board
-bool board::insideBoard(int i, int j) 
+bool board::insideBoard(int i, int j) // READY
 {
 	if(i>0&&i<9&&j>0&&j<9)
 		return true;
-	
+
 	return false;
 }
 
 //checks if the checker in position (i,j) can make a jump move. Looks if the ending position is inside the board, and if there is a checker to jump.
-bool board::canJump(int directionI, int directionJ, int i, int j, int color)
+bool board::canJump(int directionI, int directionJ, int i, int j, int color) // READY
 {
 	if(insideBoard(i+directionI,j+directionJ)&&
-	   (boardMatrix[i+directionI][j+directionJ]==(2-color)||boardMatrix[i+directionI][j+directionJ]==(4-color))&&
-	   insideBoard(i+2*directionI,j+2*directionJ)&&
-	   boardMatrix[i+2*directionI][j+2*directionJ]==0)
+			(boardMatrix[i+directionI][j+directionJ]==(2-color)||boardMatrix[i+directionI][j+directionJ]==(4-color))&&
+			insideBoard(i+2*directionI,j+2*directionJ)&&
+			boardMatrix[i+2*directionI][j+2*directionJ]==EMPTY)
 		return true;
 	else
 		return false;
@@ -95,22 +100,21 @@ bool board::canJump(int directionI, int directionJ, int i, int j, int color)
 //how many jumps can be made in a single move, 0 = white and 1 = black
 //sequence is the sequence of moves the player has to make
 //type 0=man, 1=king
-void board::calculateJumps(int i, int j, int sequence, int depth,int color, int type)
+void board::calculateJumps(int i, int j, int sequence, int depth,int color, int type)// NO FIX NEEDED
 {
 	int start,shift;
-	
-	if(depth!=1&&sequence/pow(10,depth)==(1+!(color)*7))
-	{	
+
+	if(type==0&&depth!=1&&sequence/pow(10,depth)==(1+!(color)*7))//checks if the piece is at the end of the board
+	{
 		jumpMoves.insert(sequence);
 		return;
 	}
-		
-		
-	if(type==0&&color==0)
+
+	if(type==0&&color==WHITE)
 	{
 		start=0;
 		shift=2;
-	}else if(type==0&&color==1)
+	}else if(type==0&&color==BLACK)
 	{
 		start=1;
 		shift=2;
@@ -119,17 +123,17 @@ void board::calculateJumps(int i, int j, int sequence, int depth,int color, int 
 		start=0;
 		shift=1;
 	}
-	
+
 	int directionI,directionJ; //to store in which direction we are going to move
-	
+
 	for(int loop=start;loop<4;loop=loop+shift) //we check the for possible jumping directions
 	{
 		directionI=1-(loop%2)*2;//it will take the values 1 and -1
 		directionJ=1-(loop/2)*2;//it will take the values 1 and -1
-		
-		if(canJump(directionI,directionJ,i,j,color))//if there is a jumping move 
+
+		if(canJump(directionI,directionJ,i,j,color))//if there is a jumping move
 		{
-			calculateJumps(i+2*directionI,j+2*directionJ,sequence+(j+2*directionJ)*(pow(10,depth+1))+(i+2*directionI)*(pow(10,depth+2)),depth+1, color,type ); //recursively check if we can extend the move 
+			calculateJumps(i+2*directionI,j+2*directionJ,sequence+(j+2*directionJ)*(pow(10,depth+1))+(i+2*directionI)*(pow(10,depth+2)),depth+1, color,type ); //recursively check if we can extend the move
 		}
 		else if(depth!=1)
 		{
@@ -140,116 +144,110 @@ void board::calculateJumps(int i, int j, int sequence, int depth,int color, int 
 }
 
 //calculates all the possible moves
-void board::calculatePossibleMoves(int color)
+void board::calculatePossibleMoves(int color) //READY!
 {
 	int i,j;
 	forcedToMove=0;
 	std::set<int>::iterator it;
 	std::set<int> *auxiliaryCheckerList;
-	
+
 	//depending on the color is the list it's going to check
-	if(color==0)
-		auxiliaryCheckerList=&whiteCheckers;//this should be right, have to check it later
-	else
-		auxiliaryCheckerList=&blackCheckers;
-	
+	auxiliaryCheckerList=&(checkers[color]);//this should be right, have to check it later
+
+
 	//checks the moves for all white checkers
 	for ( it=auxiliaryCheckerList->begin() ; it != auxiliaryCheckerList->end(); it++ )
-	{	
+	{
 		i=(*it) /10;
 		j=(*it) %10;
-		
-		
+
+
 		if(!forcedToMove)//checks for no jumping moves
 		{
 			if(insideBoard(i+1-2*color,j+1)&&boardMatrix[i+1-2*color][j+1]==0)//if next position is inside the board and is unoccupied we store the move
 				moves.insert((i*10)+j+((i+1-2*color)*1000)+(j+1)*100);
-				
+
 			if(insideBoard(i+1-2*color,j-1)&&boardMatrix[i+1-2*color][j-1]==0)//if next position is inside the board and is unoccupied we store the move
 				moves.insert((i*10)+j+((i+1-2*color)*1000)+(j-1)*100);
 		}
-		
-		calculateJumps(i,j,(*it),1,color,0);	
+
+		calculateJumps(i,j,(*it),1,color,0);
 	}
-	
-	if(color==0)
-		auxiliaryCheckerList=&whiteKings;//this should be right, have to check it later
-	else
-		auxiliaryCheckerList=&blackKings;
-	
+
+	auxiliaryCheckerList=&(kings[color]);//this should be right, have to check it later
+
+
 	//checks the moves for all white kings
 	for ( it=auxiliaryCheckerList->begin() ; it != auxiliaryCheckerList->end(); it++ )
-	{		
+	{
 		i=(*it) /10;
 		j=(*it) %10;
-		
+
 		//checks for no jumping moves
 		if(!forcedToMove)
 		{
 			if(i+1<9&&j+1<9&&boardMatrix[i+1][j+1]==0)//if next position is inside the board and is unoccupied we store the move
 				moves.insert((i*10)+j+((i+1)*1000)+(j+1)*100);
-				
+
 			if(i+1<9&&j-1>0&&boardMatrix[i+1][j-1]==0)//if next position is inside the board and is unoccupied we store the move
 				moves.insert((i*10)+j+((i+1)*1000)+(j-1)*100);
-			
+
 			if(i-1>0&&j+1<9&&boardMatrix[i-1][j+1]==0)//if next position is inside the board and is unoccupied we store the move
 				moves.insert((i*10)+j+((i-1)*1000)+(j+1)*100);
-				
+
 			if(i-1>0&&j-1>0&&boardMatrix[i-1][j-1]==0)//if next position is inside the board and is unoccupied we store the move
 				moves.insert((i*10)+j+((i-1)*1000)+(j-1)*100);
 		}
-		
-		
-		calculateJumps(i,j,(*it),1,color,1);		
+
+		calculateJumps(i,j,(*it),1,color,1);
 	}
 }
 
-std::set<int> board::getPossibleMoves(int color)
+std::set<unsigned int> board::getPossibleMoves(int color)// READY
 {
 	calculatePossibleMoves(color);
-	
+
 	if(!jumpMoves.empty())
-	{							
-		return jumpMoves;			
+	{
+		return jumpMoves;
 	}
-	
+
 	return moves;
 }
 
-void board::clean()
+void board::clean()//READY!
 {
 	forcedToMove=0;
 	moves.clear();
 	jumpMoves.clear();
 }
 
+void board::undo()
+{
+
+}
+
 bool board::movePiece(int sequence, int color)//sequence is the sequence of numbers that represent the move, color=0 whiteMoves, color=1 blackMoves
 {
 	int initialPosition,finalPosition,enemyPosition;
 	std::set<int> *auxiliaryCheckerList,*auxiliaryEnemyList;
-	
+
 	if(jumpMoves.empty()&&moves.count(sequence)>0)
 	{
 		initialPosition = (sequence%10)+((sequence/10)%10)*10;
 		finalPosition = (sequence/100)%10+(sequence/1000)*10;
-		
+
 		if(boardMatrix[initialPosition/10][initialPosition%10]==(1+color))
 		{
-			if(color==0)
-				auxiliaryCheckerList=&whiteCheckers;//this should be right, have to check it later
-			else
-				auxiliaryCheckerList=&blackCheckers;
-			
-			
+			auxiliaryCheckerList=&(checkers[color]);//this should be right, have to check it later
+
 			auxiliaryCheckerList->erase(initialPosition);
-			
+
 			if(finalPosition/10==(1+!(color)*7))
 			{
-				if(color==0)
-					whiteKings.insert(finalPosition);
-				else
-					blackKings.insert(finalPosition);
+				(kings[color]).insert(finalPosition);
 				boardMatrix[finalPosition/10][finalPosition%10]=3+color;
+
 			}else
 			{
 				auxiliaryCheckerList->insert(finalPosition);
@@ -259,21 +257,19 @@ bool board::movePiece(int sequence, int color)//sequence is the sequence of numb
 		}
 		else
 		{
-			if(color==0)
-				auxiliaryCheckerList=&whiteKings;//this should be right, have to check it later
-			else
-				auxiliaryCheckerList=&blackKings;
-			
+
+			auxiliaryCheckerList=&(kings[color]);//this should be right, have to check it later
+
 			auxiliaryCheckerList->erase(initialPosition);
 			auxiliaryCheckerList->insert(finalPosition);
 			boardMatrix[initialPosition/10][initialPosition%10]=0;
 			boardMatrix[finalPosition/10][finalPosition%10]=3+color;
 		}
-		
+
 		clean();
-		
+
 		return true;
-		
+
 	}else if(jumpMoves.count(sequence)>0)
 	{
 		while(sequence/100>0)
@@ -281,83 +277,61 @@ bool board::movePiece(int sequence, int color)//sequence is the sequence of numb
 			initialPosition = (sequence%10)+((sequence/10)%10)*10;
 			finalPosition = (sequence/100)%10+(sequence/1000)*10;
 			enemyPosition = 10*((initialPosition/10+finalPosition/10)/2)+(initialPosition%10+finalPosition%10)/2;
-			
+
 			if(boardMatrix[initialPosition/10][initialPosition%10]==(1+color))
 			{
-				if(color==0)
-					auxiliaryCheckerList=&whiteCheckers;//this should be right, have to check it later
-				else
-					auxiliaryCheckerList=&blackCheckers;
-				
-				
+				auxiliaryCheckerList=&(checkers[color]);//this should be right, have to check it later
+
 				auxiliaryCheckerList->erase(initialPosition);
-				
+
 				if(finalPosition/10==(1+!(color)*7))
 				{
-					if(color==0)
-						whiteKings.insert(finalPosition);
-					else
-						blackKings.insert(finalPosition);
-					
+					(kings[color]).insert(finalPosition);
 					boardMatrix[finalPosition/10][finalPosition%10]=3+color;
+
 				}else
 				{
 					auxiliaryCheckerList->insert(finalPosition);
 					boardMatrix[finalPosition/10][finalPosition%10]=1+color;
 				}
-					
+
 				boardMatrix[initialPosition/10][initialPosition%10]=0;
-				
+
 				if(boardMatrix[enemyPosition/10][enemyPosition%10]==2-color)
 				{
-					if(color==0)
-						blackCheckers.erase(enemyPosition);
-					else
-						whiteCheckers.erase(enemyPosition);
+					(checkers[!color]).erase(enemyPosition);
 				}
 				else
 				{
-					if(color==0)
-						blackKings.erase(enemyPosition);
-					else
-						whiteKings.erase(enemyPosition);
+					kings[!color].erase(enemyPosition);
 				}
-				
+
 				boardMatrix[enemyPosition/10][enemyPosition%10]=0;
 			}
 			else
 			{
-				if(color==0)
-					auxiliaryCheckerList=&whiteKings;//this should be right, have to check it later
-				else
-					auxiliaryCheckerList=&blackKings;
-				
+				auxiliaryCheckerList=&(kings[color]);//this should be right, have to check it later
+
 				auxiliaryCheckerList->erase(initialPosition);
 				auxiliaryCheckerList->insert(finalPosition);
 				boardMatrix[initialPosition/10][initialPosition%10]=0;
 				boardMatrix[finalPosition/10][finalPosition%10]=3+color;
-				
+
 				if(boardMatrix[enemyPosition/10][enemyPosition%10]==2-color)
 				{
-					if(color==0)
-						blackCheckers.erase(enemyPosition);
-					else
-						whiteCheckers.erase(enemyPosition);
+					(checkers[!color]).erase(enemyPosition);
 				}
 				else
 				{
-					if(color==0)
-						blackKings.erase(enemyPosition);
-					else
-						whiteKings.erase(enemyPosition);
+					(kings[!color]).erase(enemyPosition);
 				}
-				
+
 				boardMatrix[enemyPosition/10][enemyPosition%10]=0;
 			}
-			
+
 			sequence=sequence/100;
 		}
-			
+
 		clean();
 		return true;
 	}
--- trunk/board.hpp
+++ trunk/board.hpp
@@ -2,23 +2,31 @@
 /*
  * checkers
  * Copyright (C)  2008 <>
- * 
+ *
  * checkers is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * checkers is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef _BOARD_HPP_
 #define _BOARD_HPP_
+
+#define WHITEMAN 1
+#define BLACKMAN 2
+#define WHITEKING 3
+#define BLACKKING 4
+#define WHITE 0
+#define BLACK 1
+#define EMPTY 0
 
 #include <iostream>
 #include <list>
@@ -33,10 +41,11 @@ public:
 		board();// board constructor
 		const board &operator=( const board & rhs); //not implemented
 		void printBoard(); //prints the board to screen
-		std::set <int> getPossibleMoves(int color); //get the possible moves, if input is 0 all possible white moves, if input is 1 all possible black moves
-		void calculatePossibleMoves(int color); //calculates de moves but doesn't return them, it's used by get PossibleMoves
+		std::set <unsigned int> getPossibleMoves(int color); //get the possible moves, if input is 0 all possible white moves, if input is 1 all possible black moves
+		void calculatePossibleMoves(int color); //calculates the moves but doesn't return them, it's used by get PossibleMoves
 		int** getBoard();//returns the board in the form of an array
 		bool movePiece(int sequence,int color);//moves a checker (not implemented, YET)
+		void undoMove();//undo the last move, erasing the current state;
 
 protected:
 
@@ -44,15 +53,17 @@ private:
 		bool canJump(int directionI, int directionJ, int i, int j,int color);//sees if there is a possibility for a jump
 		bool insideBoard(int i, int j); //checks if the postion is inside the board
 		void calculateJumps(int i, int j,int sequence, int depth, int color, int type);//how many jumps can be made in a single move
+		void clean();
 		int forcedToMove; //if one there is at least one move were there is a jump
-		std::set<int> moves; //list of all moves
-		std::set<int> jumpMoves; //list of jumps moves
+		std::set<unsigned int> moves; //list of all moves
+		std::set<unsigned int> jumpMoves; //list of jumps moves
 		int **boardMatrix; //0 = free position, 1 = normal white, 2 = normal black, 3 = king white, 4 = king black
 		std::set<int> whiteCheckers; //all whiteCheckers positions
 		std::set<int> whiteKings; //all whiteKings positions
 		std::set<int> blackCheckers; //all blackCheckers positions
 		std::set<int> blackKings; //all blackKings positions
-		void clean();
+		std::set<int> checkers[2]; //all whiteCheckers positions
+		std::set<int> kings[2]; //all whiteKings positions
 };
 
 #endif // _BOARD_HPP_
