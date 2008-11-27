9ba4ccd12a13658c62097aa89258f602baebf842
strigare <strigare@162a5ca2-ab1d-11dd-b9e7-8d5594d729c2>
2008-11-27 18:16:00 +0000

    nothing

--- trunk/Heuristic.cpp
+++ trunk/Heuristic.cpp
@@ -26,8 +26,6 @@ unsigned int Heuristic::bestMove(board* b, int color) {
 	iteration = 0;
 
 	minMax(0, MINUS_INFINITY, PLUS_INFINITY);
-	std::cout<<iteration;
-	std::cout<<actualMove;
 	return actualMove;
 }
 
@@ -37,7 +35,7 @@ int Heuristic::evaluation() {
 	int x,y;
 	std::set<int>::iterator setIterator;
 
-	
+
 	// white pieces
 	for(setIterator = (b->getWhiteCheckers()).begin(); setIterator != (b->getWhiteCheckers()).end(); ++setIterator)
 	{
@@ -45,25 +43,24 @@ int Heuristic::evaluation() {
 		// x = *setIterator % 10;    // maybe used in future
 		sumWhite += PIECE_VALUE + y*y;
 	}
-	
+
 	// white kings
-	if (!(b->getWhiteKings()).empty()) {
-		for(setIterator = (b->getWhiteKings()).begin(); setIterator != (b->getWhiteKings()).end(); ++setIterator)
-		{
-			y = *setIterator / 10;
-			x = *setIterator % 10;
-			sumWhite += KING_VALUE + y*y;
-			if (y == 1 || y == 8 || x == 1 || x == 8) {		// king in the edge
-				if (x == y)	{	// king in one of corners (11, 88)
-					sumWhite -= CORNER_PENALTY;	
-				}
-				else {
-					sumWhite -= EDGE_PENALTY;
-				}
-			}	
+	if(!(b->getWhiteKings()).empty())
+	for(setIterator = (b->getWhiteKings()).begin(); setIterator != (b->getWhiteKings()).end(); ++setIterator)
+	{
+		y = *setIterator / 10;
+		x = *setIterator % 10;
+		sumWhite += KING_VALUE + y*y;
+		if (y == 1 || y == 8 || x == 1 || x == 8) {		// king in the edge
+			if (x == y)	{	// king in one of corners (11, 88)
+				sumWhite -= CORNER_PENALTY;
+			}
+			else {
+				sumWhite -= EDGE_PENALTY;
+			}
 		}
 	}
-	
+
 	// black pieces
 	for(setIterator = (b->getBlackCheckers()).begin(); setIterator != (b->getBlackCheckers()).end(); ++setIterator)
 	{
@@ -73,23 +70,22 @@ int Heuristic::evaluation() {
 	}
 
 	// black kings
-	if (!(b->getBlackKings()).empty()) {
-		for(setIterator = (b->getBlackKings()).begin(); setIterator != (b->getBlackKings()).end(); ++setIterator)
-		{
-			y = 9 - (*setIterator / 10);
-			x = *setIterator % 10;
-			sumBlack += KING_VALUE + y*y;
-			if (y == 1 || y == 8 || x == 1 || x == 8) {		// king in the edge
-				if (x == y)	{	// king in one of corners (11, 88)
-					sumBlack -= CORNER_PENALTY;	
-				}
-				else {
-					sumBlack -= EDGE_PENALTY;
-				}
+	if(!(b->getBlackKings()).empty())
+	for(setIterator = (b->getBlackKings()).begin(); setIterator != (b->getBlackKings()).end(); ++setIterator)
+	{
+		y = 9 - (*setIterator / 10);
+		x = *setIterator % 10;
+		sumBlack += KING_VALUE + y*y;
+		if (y == 1 || y == 8 || x == 1 || x == 8) {		// king in the edge
+			if (x == y)	{	// king in one of corners (11, 88)
+				sumBlack -= CORNER_PENALTY;
+			}
+			else {
+				sumBlack -= EDGE_PENALTY;
 			}
 		}
 	}
-	
+
 	// checking which player AI is playing
 	if (color == WHITE) {
 		return sumWhite - sumBlack;
@@ -97,19 +93,18 @@ int Heuristic::evaluation() {
 	else {
 		return sumBlack - sumWhite;
 	}
-	
+
 }
 
 int Heuristic::minMax(int level, int alpha, int beta) {
         int tmp = 0;    // temporary value which is compared to max value
-        int winCheck=0;  
+        int winCheck=0;
         set<unsigned int> tempMoveSet;
-        set<unsigned int>::iterator iter, iterEnd;      
-       
+        set<unsigned int>::iterator iter, iterEnd;
+
         iteration++;
-        std::cout<<"iteration "<<iteration;
-        
-        b->calculatePossibleMoves(color);
+       // std::cout<<"iteration "<<iteration;
+
         winCheck = b->terminal();
         if (winCheck == color+2)     // computer wins
             return PLUS_INFINITY;
@@ -117,56 +112,63 @@ int Heuristic::minMax(int level, int alpha, int beta) {
             return 0;
         if (winCheck != 0)              // opponent wins
             return MINUS_INFINITY;
-        
-        
+
+
         // Leaf
         if (level == maxLevel) {
-        	std::cout<<" evaluation: "<<evaluation();
+        	//evaluation();
         	return evaluation();
         }
-        
-     
-        // MAX       
+
+        // MAX
         if (level % 2 == 0) {
-	       // getting possible moves  
-	        tempMoveSet = b->getPossibleMoves(color);
-			iter = tempMoveSet.begin();
-			iterEnd = tempMoveSet.end();
+
+        	// getting possible moves
+        	b->calculatePossibleMoves(color);
+        	tempMoveSet = b->getPossibleMoves(color);
+       		iter = tempMoveSet.begin();
+   			iterEnd = tempMoveSet.end();
+
             for (; iter != iterEnd; ++iter) {
         	    if (alpha >= beta)      // while (alpha < beta)
                 	break;
                 b->movePiece(*iter, color);
                 tmp = minMax(level+1, alpha, beta);
                 b->undoMove();
+                b->calculatePossibleMoves(color);
                 if (tmp > alpha) {
         	        alpha = tmp;
         	    	if (level == 0) {
         	    		actualMove = *iter;
         	    	}
-                }                
+                }
             }
             return alpha;
         }
 
-        // MIN       
+        // MIN
        else {
-	        // getting possible moves  
-	        tempMoveSet = b->getPossibleMoves(!color);
-			iter = tempMoveSet.begin();
-			iterEnd = tempMoveSet.end();       	
+
+    	    // getting possible moves
+    	    b->calculatePossibleMoves(!color);
+            tempMoveSet = b->getPossibleMoves(!color);
+    	   	iter = tempMoveSet.begin();
+    	   	iterEnd = tempMoveSet.end();
+
             for (; iter != iterEnd; ++iter) {
         	    if (alpha >= beta)      // while (alpha < beta)
                 	break;
                 b->movePiece(*iter, !color);
                 tmp = minMax(level+1, alpha, beta);
                 b->undoMove();
+                b->calculatePossibleMoves(!color);
                 if (tmp < beta) {
         	        beta = tmp;
-                }                
+                }
             }
             return beta;
         }
-        
+
 }
 
 
